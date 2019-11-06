2fe4e9ceb6078000656406e8d10fc1c199cfe6ff
sarahdrama <sarah@tecsi.de>
2019-11-06 19:52:53 +0100

    added main-page and cookie-policy components

---
+++ src/app/cookie-policy/cookie-policy.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cookie-policy',
+  templateUrl: './cookie-policy.component.html',
+  styleUrls: ['./cookie-policy.component.css']
+})
+export class CookiePolicyComponent {
+
+  constructor() { }
+
+}
---
+++ src/app/main-page/main-page.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {CookieAComponent} from '../cookie-banner/cookie-a/cookie-a.component';
+import {CookieBComponent} from '../cookie-banner/cookie-b/cookie-b.component';
+import {SurveyStateService} from '../service/survey-state.service';
+import {MatDialog, MatSnackBar} from '@angular/material';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.css']
+})
+export class MainPageComponent implements OnInit {
+
+  formHidden = true;
+
+  constructor(
+    public surveyStateService: SurveyStateService,
+    public dialog: MatDialog,
+    public snackbar: MatSnackBar) { }
+
+  ngOnInit() {
+    this.surveyStateService.data.timeJoined = Date.now();
+    if (Math.floor(Math.random() * 2) === 0) {
+      this.surveyStateService.data.batch = 'A';
+      this.snackbar.openFromComponent(CookieAComponent);
+    } else {
+      this.surveyStateService.data.batch = 'B';
+      this.dialog.open(CookieBComponent, { disableClose: true } );
+    }
+  }
+
+  startSurvey() {
+    this.formHidden = false;
+  }
+
+}
